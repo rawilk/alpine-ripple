c5c747a156d7f253e1a40ee436fe3758194a750a
Randall Wilk <wilk.randall@gmail.com>
2022-12-15 15:23:31 -0600

    Add scripts and styles

---
+++ src/js/ripple.js
@@ -0,0 +1,69 @@
+import { getCustomColorFromModifiers, getCustomRadiusFromModifiers, willHaveAMouseUpEvent } from './utils';
+
+export const addRipple = (event, el, modifiers) => {
+    if (! willHaveAMouseUpEvent(event)) {
+        return;
+    }
+
+    const ripple = document.createElement('div');
+    ripple.classList.add('ripple');
+
+    el.appendChild(ripple);
+
+    const size = ripple.offsetWidth,
+        position = ripple.getBoundingClientRect(),
+        innerRipple = document.createElement('span');
+
+    const x = event.pageX - position.left - (size / 2),
+        y = event.pageY - position.top - (size / 2);
+
+    let style = `top: ${y}px; left: ${x}px; width: ${size}px; height: ${size}px;`;
+
+    const color = getCustomColorFromModifiers(modifiers);
+    if (color.indexOf('bg-') === 0) {
+        // Prefix with '!' for !important.
+        innerRipple.classList.add(`!${color}`);
+    } else if (color.indexOf('#') === 0 || color.indexOf('rgb') === 0) {
+        style += `background-color: ${color};`;
+    }
+
+    const radius = getCustomRadiusFromModifiers(modifiers);
+    if (radius) {
+        style += `border-radius: ${radius};`;
+    }
+
+    ripple.appendChild(innerRipple);
+    innerRipple.setAttribute('style', style);
+};
+
+/**
+ * Remove the ripple from the element.
+ *
+ * @param {HTMLElement} el
+ */
+export const removeRipple = el => {
+    setTimeout(() => {
+        // We are only removing the first instance to prevent ripples from subsequent clicks
+        // being removed too quickly before the ripple effect can properly be seen.
+        const ripple = el.querySelector('.ripple');
+
+        ripple && ripple.remove();
+    }, 1000);
+};
+
+function Ripple(Alpine) {
+    Alpine.directive('ripple', (el, { modifiers, expression }, { cleanup }) => {
+        const clickHandler = event => addRipple(event, el, modifiers);
+        const mouseUpHandler = () => removeRipple(el);
+
+        el.addEventListener('mousedown', clickHandler);
+        el.addEventListener('mouseup', mouseUpHandler);
+
+        cleanup(() => {
+            el.removeEventListener('mousedown', clickHandler);
+            el.removeEventListener('mouseup', mouseUpHandler);
+        });
+    });
+}
+
+export default Ripple;
---
+++ src/js/utils.js
@@ -0,0 +1,50 @@
+export const getCustomColorFromModifiers = modifiers => {
+    if (! modifiers.includes('color')) {
+        return '';
+    }
+
+    const nextModifier = modifiers[modifiers.indexOf('color') + 1] || 'invalid-color';
+    if (nextModifier.indexOf('#') === 0 || nextModifier.indexOf('rgb') === 0) {
+        return nextModifier;
+    }
+
+    return nextModifier.indexOf('bg') === 0
+        ? nextModifier
+        : `bg-${nextModifier}`;
+};
+
+export const getCustomRadiusFromModifiers = modifiers => {
+    if (! modifiers.includes('radius')) {
+        return '';
+    }
+
+    let nextModifier = modifiers[modifiers.indexOf('radius') + 1] || 'invalid-radius';
+
+    // _ allows us to use decimals, such as 0.5.
+    nextModifier = nextModifier.replace('_', '.');
+
+    // Separate the numeric value from the unit in nextModifier.
+    // Possible values for nextModifier: 50, 50.5, 50.5px, 50px, 50%, 50rem, 50em
+    const numericValue = nextModifier.match(/^[0-9]+(\.[0-9]+)?/)[0];
+    let unit = nextModifier.replace(numericValue, '');
+    if (! unit) {
+        unit = '%';
+    }
+
+    return `${numericValue}${unit}`;
+}
+
+/**
+ * Some events, such as a right click or ctrl + left click won't trigger a mouseup event,
+ * so we need to prevent the ripple from being added in those cases.
+ *
+ * @param {MouseEvent} event
+ * @returns {boolean}
+ */
+export const willHaveAMouseUpEvent = event => {
+    if (event.ctrlKey) {
+        return false;
+    }
+
+    return event.button === 0 || event.button === 1;
+};
